cde3855d541a93a640a6d3f5ee5dd7f00b32de2b
taylor <taylor.laekeman@gmail.com>
2024-09-17 22:11:35 -0400

    remove broken css import to fix build

--- src/app/randomnote/page.tsx
+++ src/app/randomnote/page.tsx
@@ -1,11 +1,6 @@
 'use client';
-import styles from '@/app/page.module.css';
 import RandomNote from '@/components/RandomNote';
 
 export default function Page() {
-  return (
-    <main className={styles.main}>
-      <RandomNote />
-    </main>
-  );
+  return <RandomNote />;
 }

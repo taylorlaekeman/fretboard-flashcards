e70c6fab60366324cec72a0a6f29026eeb5735d6
taylor <taylor.laekeman@gmail.com>
2024-10-03 21:04:01 -0400

    update title and remove favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Guitar Dojo',
+  title: 'Fretboard',
   description: 'Hone your fills',
 };
 
